05d7435bd832b4a4f5ed581cc0a64eafa25b54bf
Martin Jakal <martinjakal93@gmail.com>
2022-12-08 17:52:25 +0100

    Add parent to binary node

--- node/BinaryNode.hpp
+++ node/BinaryNode.hpp
@@ -4,7 +4,7 @@
 #include "NodeProperties.hpp"
 
 template <typename T>
-class BinaryNode : public Node<T>, public HasDoubleChild<BinaryNode<T>>
+class BinaryNode : public Node<T>, public HasParent<BinaryNode<T>>, public HasDoubleChild<BinaryNode<T>>
 {
 public:
     using Base = Node<T>;
